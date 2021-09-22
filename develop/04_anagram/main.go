96f986d627bc218b5ad6eadae810ebd8415356d8
Pogosyan Sos <pogosyan.sos@wb.ru>
2021-09-22 23:14:37 +0300

    Continue add tests...

--- develop/04_anagram/main.go
+++ develop/04_anagram/main.go
@@ -6,24 +6,24 @@ import (
 	"strings"
 )
 
-// sortCharsInWord сортирует символы в слове для построения корректной map
-func sortCharsInWord(s string) string {
+// SortCharsInWord сортирует символы в слове для построения корректной map
+func SortCharsInWord(s string) string {
 	s = strings.ToLower(s)
-	container  := strings.Split(s, "")
+	container := strings.Split(s, "")
 	sort.Strings(container)
 	return strings.Join(container, "")
 }
 
-// builtSet формирует слайс только из уникальных значений
-func builtSet(bucket []string) []string{
+// BuiltSet формирует слайс только из уникальных значений
+func BuiltSet(bucket []string) []string {
 	// Реализовали set
 	set := make(map[string]bool)
 	var setToSlice []string
-	for _, v := range bucket{
+	for _, v := range bucket {
 		v = strings.ToLower(v)
 		set[v] = true
 	}
-	for i := range set{
+	for i := range set {
 		setToSlice = append(setToSlice, i)
 	}
 	sort.Strings(setToSlice)
@@ -32,26 +32,38 @@ func builtSet(bucket []string) []string{
 
 // Функция printAnagrams предназначена для красивой печати
 func printAnagrams(bucket map[string][]string) {
-	for k, v := range bucket{
+	for k, v := range bucket {
 		fmt.Printf("%v : %v\n", k, v)
 	}
 }
 
-// finedAnagram является главной функцией, которая ищет во входном массиве анаграммы
-func finedAnagram(arr *[]string) {
-	bucket := map[string][]string{}
-	for _, v := range *arr{
-		normal := sortCharsInWord(v)
+// FinedAnagram является главной функцией, которая ищет во входном массиве анаграммы
+func FinedAnagram(arr *[]string) map[string][]string {
+	bucket := make(map[string][]string, 0)
+	keys := make(map[string][]int, 0)
+	res := make(map[string][]string, 0)
+	buck := make([]string, 0)
+
+	for i, v := range *arr {
+		normal := SortCharsInWord(v)
+		keys[normal] = append(keys[normal], i)
 		bucket[normal] = append(bucket[normal], v)
 	}
 
-	for k, v := range bucket {
-		bucket[k] = builtSet(v)
+	for k, _ := range bucket {
+		buck = append(buck, k)
 	}
-	printAnagrams(bucket)
+
+	for _, v := range buck {
+		min := keys[v][0]
+		res[(*arr)[min]] = BuiltSet(bucket[v])
+	}
+
+	return res
 }
 
-func main(){
+func main() {
 	words := []string{"пятка", "пятак", "тяпка", "листок", "слиток", "столик", "ЛИСТОК", "ТяПкА"}
-	finedAnagram(&words)
+	anagrams := FinedAnagram(&words)
+	printAnagrams(anagrams)
 }
---
+++ develop/04_anagram/main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSortCharsInWords(t *testing.T) {
+	table := []struct {
+		in  string
+		out string
+	}{
+		{
+			"дгвба",
+			"абвгд",
+		},
+		{
+			"привет",
+			"веипрт",
+		},
+		{
+			"Hello",
+			"ehllo",
+		},
+	}
+
+	for _, v := range table {
+		result := SortCharsInWord(v.in)
+		assert.Equal(t, result, v.out)
+	}
+}
+
+func TestBuiltSet(t *testing.T) {
+	table := []struct {
+		in  []string
+		out []string
+	}{
+		{
+			[]string{"Hello", "World", "hello", "WoRlD"},
+			[]string{"hello", "world"},
+		},
+		{
+			[]string{"One", "two", "thrEe", "onE", "TWO", "three"},
+			[]string{"one", "three", "two"},
+		},
+		{
+			[]string{"Bred", "banana", "tomato", "tomato", "BANANA", "Ban", "BanaN", "TOMATO"},
+			[]string{"ban", "banan", "banana", "bred", "tomato"},
+		},
+	}
+
+	for _, v := range table {
+		result := BuiltSet(v.in)
+		assert.Equal(t, result, v.out)
+	}
+}
+
+func TestFinedAnagram(t *testing.T) {
+	table := []struct {
+		in  *[]string
+		out map[string][]string
+	}{
+		{
+			&[]string{"пятка", "пятак", "тяпка", "листок", "слиток", "столик", "ЛИСТОК", "ТяПкА"},
+			map[string][]string{
+				"пятка":  []string{"пятак", "пятка", "тяпка"},
+				"листок": []string{"листок", "слиток", "столик"},
+			},
+		},
+		{
+			&[]string{"импортер", "пирометр", "реимпорт", "каретник", "катерник", "кретинка"},
+			map[string][]string{
+				"импортер" : []string{"импортер", "пирометр", "реимпорт"},
+				"каретник" : []string{"каретник", "катерник", "кретинка"},
+			},
+		},
+		{
+			&[]string{"шрам", "лоза", "зало", "Зола", "лОза", "марш", "шарм", "ШрАм"},
+			map[string][]string{
+				"лоза" : []string{"зало", "зола", "лоза"},
+				"шрам" : []string{"марш", "шарм", "шрам"},
+			},
+		},
+	}
+
+	for _, v := range table {
+		result := FinedAnagram(v.in)
+		assert.Equal(t, result, v.out)
+	}
+}
